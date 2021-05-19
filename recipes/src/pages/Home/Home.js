c78bcd882d5f8c4cbb691c9d8e2ae4d4a603e2f7
sagar-aryal <sagar_yal92@outlook.com>
2021-05-19 09:02:35 +0300

    clone and repushed fixing github error.

---
+++ recipes/src/pages/Home/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import Cooking from "../../assets/home.mp4";
+
+const Home = () => {
+  const history = useHistory();
+  return (
+    <div className="home">
+      <div className="caption">
+        <h2>Nothing Brings You Together Like Good Food</h2>
+        <h3>Explore your choices</h3>
+        <button
+          className="btn"
+          onClick={() => {
+            history.push("/recipes");
+          }}
+        >
+          Explore
+        </button>
+      </div>
+
+      <video loop muted autoPlay preload="auto">
+        <source src={Cooking} type="video/mp4" />
+      </video>
+    </div>
+  );
+};
+
+export default Home;
